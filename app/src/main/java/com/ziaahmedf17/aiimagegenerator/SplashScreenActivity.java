13b24076f57d98925d063881da295f75d4c35e76
Zia Ahmed <ziaahmedf17@gmail.com>
2023-10-31 06:20:48 +0500

    Layout Designing

--- app/src/main/java/com/ziaahmedf17/aiimagegenerator/SplashScreenActivity.java
+++ app/src/main/java/com/ziaahmedf17/aiimagegenerator/SplashScreenActivity.java
@@ -2,7 +2,9 @@ package com.ziaahmedf17.aiimagegenerator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+//        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
     }
 }
